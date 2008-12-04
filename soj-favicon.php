9d4ed21e73d24192c0ef21e50032fb9345a462a9
sojweb <sojweb@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-12-04 15:34:08 +0000

    tagging version 1.0.2
    git-svn-id: https://plugins.svn.wordpress.org/soj-favicon/tags/1.0.2@77504 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ soj-favicon.php
@@ -0,0 +1,21 @@
+<?php
+/*
+Plugin Name: SoJ Favicon
+Plugin URI: http://journalism.indiana.edu/apps/mediawiki-1.10.1/index.php/Wp_favicon
+Description: Changes the Favicon for WordPress admin pages
+Author: Jeff Johnson
+Version: 1.0.2
+*/
+
+function favicon()
+{
+	$u = get_bloginfo('siteurl');
+	echo '
+    <link rel="shortcut icon" href="'.$u.'/wp-content/plugins/soj-favicon/favicon.ico" type="image/vnd.microsoft.icon" />
+    <link rel="icon" href="'.$u.'/wp-content/plugins/soj-favicon/favicon.ico" type="image/vnd.microsoft.icon" /> 
+';
+}
+
+add_action('admin_head', 'favicon');
+
+?>
